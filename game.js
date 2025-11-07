6ad1c0560a9900daf99f32f995afb7ab49f006c9
Jelena Starostina <jelena.starostina@ivkhk.ee>
2025-11-07 19:52:59 +0200

    расставить объекты на экране

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 let game = {
     ctx: null,
+    platform: null,
+    ball: null,
     sprites: {
         background: null,
         ball: null,
@@ -15,7 +17,7 @@ let game = {
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = "img/" + key + ".png";
-            this.sprites[key].addEventListener("load", () =>{
+            this.sprites[key].addEventListener("load", () => {
                 ++loaded;
                 if (loaded >= required) {
                     callback();
@@ -30,8 +32,9 @@ let game = {
     },
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0);
-        this.ctx.drawImage(this.sprites.platform, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
+            this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     },
     start: function() {
         this.init();
@@ -40,8 +43,22 @@ let game = {
         });
     }
 };
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    x: 280,
+    y: 300
+};
+
 window.addEventListener("load", () => {
     game.start();
 });
+
 
 
