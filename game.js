3d56f5e134d1fae3b08f74d12f5cb87c568d3342
Jelena Starostina <jelena.starostina@ivkhk.ee>
2025-11-21 18:56:37 +0200

    Отскок мяча от платформы

--- game.js
+++ game.js
@@ -69,11 +69,20 @@ let game = {
   update() {
     this.platform.move();
     this.ball.move();
+    this.collideBlocks();
+    this.collidePlatform();
+  },
+  collideBlocks() {
 
     for (let block of this.blocks) {
       if (this.ball.collide(block)) {
         this.ball.bumpBlock(block);
       }
+    }
+  },
+  collidePlatform() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumpPlatform(this.platform);
     }
   },
   run() {
@@ -142,6 +151,11 @@ game.ball = {
       },
       bumpBlock(block) {
         this.dy *= -1;
+      },
+      bumpPlatform(platform) {
+        this.dy *= -1;
+        let touchX = this.x + this.width / 2;
+        this.dx = this.velocity * platform.getTouchOffset(touchX);
       }
 };
 
@@ -150,6 +164,8 @@ game.platform = {
   dx: 0,
     x: 280,
     y: 300,
+    width: 100,
+    height: 14,
     ball: game.ball,
     fire() {
       if (this.ball) {
@@ -176,6 +192,12 @@ game.platform = {
           this.ball.x += this.dx;
         }
       }
+    },
+    getTouchOffset(x) {
+      let diff = (this.x + this.width) - x;
+      let offset = this.width - diff;
+      let result = 2 * offset / this.width;
+      return result -1;
     }
 };
 
