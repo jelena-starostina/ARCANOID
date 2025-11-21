1f8da95de36bd24d1ac05476f47d25a6d7167f5b
Jelena Starostina <jelena.starostina@ivkhk.ee>
2025-11-21 18:06:20 +0200

    Обработка столкновения мяча с блоками

--- game.js
+++ game.js
@@ -43,6 +43,9 @@ let game = {
       if (loaded >= required) {
         callback();
       }
+      if (loaded >= required) {
+        callback();
+      }
     };
 
     for (let key in this.sprites) {
@@ -55,6 +58,8 @@ let game = {
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         this.blocks.push({
+          width: 60,
+          height: 20,
           x: 64 * col + 65,
           y: 24 * row + 35
         });
@@ -64,6 +69,12 @@ let game = {
   update() {
     this.platform.move();
     this.ball.move();
+
+    for (let block of this.blocks) {
+      if (this.ball.collide(block)) {
+        this.ball.bumpBlock(block);
+      }
+    }
   },
   run() {
     window.requestAnimationFrame(() => {
@@ -91,7 +102,7 @@ let game = {
         this.run();
     });
 },
-randon(min, max) {
+random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 };
@@ -106,16 +117,31 @@ game.ball = {
     height: 20,
     start() {
         this.dy = -this.velocity;
-        this.dx = game.randon(-this.velocity, this.velocity);
+        this.dx = game.random(-this.velocity, this.velocity);
       },
       move() {
         if (this.dy) {
           this.y += this.dy;
         }
         if (this.dx) {
-          this.x += this.dy;
+          this.x += this.dx;
         }
 
+      },
+      collide(element) {
+        let x = this.x + this.dx;
+        let y = this.y + this.dy;
+
+        if (x + this.width > element.x &&
+          x < element.x + element.width &&
+          y + this.height > element.y &&
+          y < element.y + element.height) {
+            return true;
+          }
+          return false;
+      },
+      bumpBlock(block) {
+        this.dy *= -1;
       }
 };
 
