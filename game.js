8094e1602611a89cd39e5c5803eb57fb8b07ee34
Jelena Starostina <jelena.starostina@ivkhk.ee>
2025-12-01 19:29:35 +0200

    Не даём платформе выйти за края экрана

--- game.js
+++ game.js
@@ -69,6 +69,7 @@ let game = {
     this.collideBlocks();
     this.collidePlatform();
     this.ball.collideWorldBounds();
+    this.platform.collideWorldBounds();
     this.platform.move();
     this.ball.move();
     
@@ -185,6 +186,10 @@ game.ball = {
         block.active = false;
       },
       bumpPlatform(platform) {
+        if (platform.dx) {
+          this.x += platform.dx;
+        }
+
        if (this.dy > 0) {
         this.dy = -this.velocity;
         let touchX = this.x + this.width / 2;
@@ -233,7 +238,18 @@ game.platform = {
       let offset = this.width - diff;
       let result = 2 * offset / this.width;
       return result -1;
+},
+collideWorldBounds() {
+  let x= this.x + this.dx;
+  let platformLeft = x;
+  let platformRight = platformLeft + this.width;
+  let worldLeft = 0;
+  let worldRight = game.width;
+
+  if (platformLeft < worldLeft || platformRight > worldRight) {
+    this.dx = 0;
     }
+  }
 };
 
 window.addEventListener("load", () => {
